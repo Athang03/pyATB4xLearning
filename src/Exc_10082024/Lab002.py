df1deb37e8e3dea69d48f337e8cf91b2bba3b2b1
Athang03 <siddhavinarvekar1@gmail.com>
2024-08-14 11:34:10 +0530

    GIT

---
+++ src/Exc_10082024/Lab002.py
@@ -0,0 +1,19 @@
+age = "Sid"
+print(age)
+a = 10
+print(a)
+_ = 60
+print(_)
+_ = _ + 1
+print(_)
+
+pi = 3.14
+name = "Siddhavi"
+ismale = True
+print(type(pi))
+print(type(name))
+print(type(ismale))
+
+complex_number= 2+3j
+print(complex_number.real)
+print(complex_number.imag)
---
+++ src/Exc_10082024/Lab004.py
@@ -0,0 +1,3 @@
+#another way of initializing variable
+a,b,c = 23,69,34
+print(a,b,c,sep="_")
